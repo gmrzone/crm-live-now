2acc80afd26d57e4e3f713261be56db33ad4e128
Afzal Saiyed <you@example.comsaiyedafzalgz@gmail.com>
2020-09-11 04:47:42 -0700

    initial commit

--- statics/js/signup.js
+++
@@ -1,6 +0,0 @@
-var signup_input_field = document.getElementsByTagName('input')
-console.log(signup_input_field)
-signup_input_field[1].placeholder='Username'
-signup_input_field[2].placeholder='Email'
-signup_input_field[3].placeholder='Password'
-signup_input_field[4].placeholder='Confirm Password'
